a8ce2657fffbe24fc6f9d69ef43dccddbfe9b450
Marko Mijajlovic <marko.mijajlovic@proton.me>
2024-08-29 23:02:34 +0200

    batman

---
+++ src/main/java/com/mijamarko/trezor/TrezorApplication.java
@@ -0,0 +1,13 @@
+package com.mijamarko.trezor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrezorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrezorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mijamarko/trezor/TrezorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mijamarko.trezor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrezorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
